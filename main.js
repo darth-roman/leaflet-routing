aaa0e27be2de9791eac51ffc1a1d42e9a07596e7
Darth Roman <chouchane.louai20@gmail.com>
2023-09-16 23:20:47 +0100

    Cleaner initials

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
 const map = L.map('map').setView([51.505, -0.09], 13)
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
 
-L.Routing.control({
-    waypoints: [
-      L.latLng(57.74, 11.94),
-      L.latLng(57.6792, 11.949)
-    ]
-  }).addTo(map);
+// L.Routing.control({
+//     waypoints: [
+//       L.latLng(57.74, 11.94),
+//       L.latLng(57.6792, 11.949)
+//     ]
+//   }).addTo(map);
+
+
+  const initMap = ({mapTile, mapAttribution, zoom = 13} = {}) => {
+    L.tileLayer(mapTile, {
+        maxZoom: zoom,
+        attribution: mapAttribution
+    }).addTo(map)
+
+    console.log(mapTile);
+
+    // L.Routing.control({
+    //     waypoints: [
+    //       L.latLng(57.74, 11.94),
+    //       L.latLng(57.6792, 11.949)
+    //     ]
+    //   }).addTo(map);
+    
+  }
+
+
+  const initRouting = ({waypoints}) => {
+    L.Routing.control({
+        waypoints
+      }).addTo(map);
+
+      console.log(waypoints);
+  }
+
+  initMap({
+    mapTile: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    mapAttribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+
+  initRouting({waypoints: [
+    L.latLng(57.74, 11.94),
+    L.latLng(57.6792, 11.949)
+  ]})
+ 
